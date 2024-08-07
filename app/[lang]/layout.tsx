afa9d31e001301be028c83906f4ef899608aff78
hallidayyy <22263821@qq.com>
2024-08-07 22:22:40 +0800

    remove i18n lang

--- app/[lang]/layout.tsx
+++
@@ -1,6 +0,0 @@
-// app/[lang]/layout.tsx
-import DefaultLayout from '@/app/(default)/layout';
-
-export default function LangLayout({ children }: { children: React.ReactNode }) {
-  return <DefaultLayout>{children}</DefaultLayout>;
-}
