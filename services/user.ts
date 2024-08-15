bfa39e7d47ff6cd797d9c94ba0bbefe2605436a4
hallidayyy <22263821@qq.com>
2024-08-16 00:14:17 +0800

    1111

--- services/user.ts
+++ services/user.ts
@@ -2,6 +2,10 @@ import { findUserByEmail, insertUser } from "@/models/user";
 
 import { User } from "@/types/user";
 
+/**
+ * Saves a user to the database if the user does not already exist.
+ * @param user - The user object to be saved.
+ */
 export async function saveUser(user: User) {
   try {
     const existUser = await findUserByEmail(user.email);
